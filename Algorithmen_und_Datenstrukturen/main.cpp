a84c8d793aadb6f47b9f9c0ca90ca1ddce08eae5
Norman Olejnik <norman.olejnik@alumni.fh-aachen.de>
2018-12-08 21:17:15 +0100

    Initial Commit

---
+++ Algorithmen_und_Datenstrukturen/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Algorithmen_und_Datenstrukturen
+//
+//  Created by Norman Olejnik on 08.12.18.
+//  Copyright © 2018 Norman Olejnik. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
