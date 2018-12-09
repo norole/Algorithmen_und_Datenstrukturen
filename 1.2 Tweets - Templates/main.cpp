60bce1fff1ee83bfe5d339f503fe2f821b196b60
Norman Olejnik <norman.olejnik@alumni.fh-aachen.de>
2018-12-09 15:39:35 +0100

    Projektkonstrukt erstellt.

---
+++ 1.2 Tweets - Templates/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  1.2 Tweets - Templates
+//
+//  Created by Norman Olejnik on 09.12.18.
+//  Copyright © 2018 Norman Olejnik. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ 1.3 Tweets - Realisierung/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  1.3 Tweets - Realisierung
+//
+//  Created by Norman Olejnik on 09.12.18.
+//  Copyright © 2018 Norman Olejnik. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    std::cout << "Hello, World!\n";
+    return 0;
+}
